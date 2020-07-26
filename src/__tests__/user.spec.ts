3d1305ebd1ea5c092444abebdba2136c158abdd6
kibartas <juodasis.perlas@gmail.com>
2020-07-26 21:09:31 +0300

    write first test

---
+++ src/__tests__/user.spec.ts
@@ -0,0 +1,49 @@
+import UserModel, { IUser } from "../models/user";
+import * as dbHandler from "../testUtils/db-handler";
+
+const userData = {
+  username: "JohnLukeThe3rd",
+  password: "Rabarbaras123",
+  creationDate: new Date(),
+};
+
+beforeAll(async () => await dbHandler.connect());
+
+afterEach(async () => await dbHandler.clearDatabase());
+
+afterAll(async () => dbHandler.closeDatabase());
+
+describe("User model", () => {
+  it("can be created correctly", async () => {
+    expect(await UserModel.estimatedDocumentCount()).toEqual(0);
+    await UserModel.create(userData);
+    console.log(await UserModel.findOne(userData));
+    expect(await UserModel.estimatedDocumentCount()).toEqual(1);
+  });
+
+  it("can be removed correctly", async () => {
+    expect(await UserModel.estimatedDocumentCount()).toEqual(0);
+    await UserModel.create(userData);
+    expect(await UserModel.estimatedDocumentCount()).toEqual(1);
+    await UserModel.remove(userData);
+    expect(await UserModel.estimatedDocumentCount()).toEqual(0);
+  });
+
+  it("can be updated correctly", async () => {
+    expect(await UserModel.estimatedDocumentCount()).toEqual(0);
+    await UserModel.create(userData);
+    expect(await UserModel.estimatedDocumentCount()).toEqual(1);
+    const result: IUser = <IUser>(
+      await UserModel.findOneAndUpdate(
+        { username: userData.username },
+        { username: "Kirminas" },
+        { new: true }
+      )
+    );
+    const { username, password }: IUser = result;
+    expect({ username, password }).toEqual({
+      username: "Kirminas",
+      password: userData.password,
+    });
+  });
+});
