a5864cffed2e1581d6fdc48a59aabd91ef17dc48
kibartas <juodasis.perlas@gmail.com>
2020-07-27 17:09:21 +0300

    update auth logic and add GET /user endpoint

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -2,14 +2,23 @@ import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
 
 export const auth = (req: Request, res: Response, next: NextFunction): void => {
-  const body = req.body;
-  if (!body?.token) {
+  const bearerToken = req.header("Authorization");
+  let token;
+  if (!bearerToken) {
     res.sendStatus(401);
+    return;
   } else {
+    if (!bearerToken.startsWith("Bearer ")) {
+      res.sendStatus(401);
+      return;
+    } else {
+      token = bearerToken.replace("Bearer ", "");
+    }
     try {
-      jwt.verify(body.token, process.env.TOKEN_SECRET as string);
+      jwt.verify(token, process.env.TOKEN_SECRET as string);
       next();
     } catch (e) {
+      console.log(e);
       res.sendStatus(401);
     }
   }
--- src/requests/index.ts
+++ src/requests/index.ts
@@ -1,5 +1,7 @@
 import { Application, Request, Response } from "express";
 import generateToken from "../authentication/generateToken";
+import jwt from "jsonwebtoken";
+import { jwtStruct } from "../@types";
 
 type AuthRequestBody = {
   username: string;
@@ -15,6 +17,22 @@ export default (app: Application): void => {
     } else {
       res.status(400);
       res.send("Bad request");
+    }
+  });
+
+  app.get("/user", (req: Request, res: Response): void => {
+    let token = req.header("Authorization");
+    if (token === undefined) {
+      res.sendStatus(400);
+    } else {
+      token = token.replace("Bearer ", "");
+      const payload: jwtStruct = jwt.decode(token as string) as jwtStruct;
+      if (!payload?.username) {
+        res.sendStatus(400);
+        return;
+      } else {
+        res.json({ username: payload.username });
+      }
     }
   });
 };
