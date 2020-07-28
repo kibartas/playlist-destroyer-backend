86560bd51cfd138887539a487ea316f5f168f76d
kibartas <juodasis.perlas@gmail.com>
2020-07-28 15:59:35 +0300

    remove unnecessary line from testUtils/appInit.ts

--- src/testUtils/appInit.ts
+++ src/testUtils/appInit.ts
@@ -7,6 +7,4 @@ export const appInit = (app: Application): void => {
 
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
-
-  app.set("port", process.env.PORT || 8080);
 };
