26da59f6facf0bdcc79902fd193eebe7241f73a5
kibartas <juodasis.perlas@gmail.com>
2020-07-29 14:30:46 +0300

    change generateToken function to include user role

--- src/authentication/generateToken.ts
+++ src/authentication/generateToken.ts
@@ -1,3 +1,3 @@
 import jwt from 'jsonwebtoken';
 
-export default (username: { username: string }): string => jwt.sign(username, process.env.TOKEN_SECRET as string);
+export default (userData: { username: string; role: string }): string => jwt.sign(userData, process.env.TOKEN_SECRET as string);
