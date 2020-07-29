6c60e73c82688bfc4964f29059349f7224d6f784
kibartas <juodasis.perlas@gmail.com>
2020-07-29 18:52:58 +0300

    create database init functions

---
+++ src/database/utils/init.ts
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+
+const init = {
+  connect: async (): Promise<number> => {
+    const mongooseOptions = {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    };
+    await mongoose.connect(
+      'mongodb://localhost:27017/playlist-destroyer',
+      mongooseOptions,
+    );
+    return mongoose.connection.readyState;
+  },
+  closeDatabase: async (): Promise<void> => {
+    await mongoose.connection.dropDatabase();
+    await mongoose.connection.close();
+  },
+};
+
+export default init;
