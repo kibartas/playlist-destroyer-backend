ca5638cfad15da1a84edb39af14a5bb9d47defa4
kibartas <juodasis.perlas@gmail.com>
2020-07-29 21:55:36 +0300

    make initialization func async

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,10 @@ import initialization from './initialization';
 
 const app: Application = express();
 
-initialization(app);
-
-const port = app.get('port');
-
-app.listen(port, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Server listening at port ${port}`);
+initialization(app).then(() => {
+  const port = app.get('port');
+  app.listen(port, () => {
+    // eslint-disable-next-line no-console
+    console.log(`Server listening at port ${port}`);
+  });
 });
