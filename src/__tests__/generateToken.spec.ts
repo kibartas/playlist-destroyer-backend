399cd0e819124f2245c989b2ab20603df1103c57
kibartas <juodasis.perlas@gmail.com>
2020-07-29 14:32:46 +0300

    add an expect to generateToken that expects role in the token

--- src/__tests__/generateToken.spec.ts
+++ src/__tests__/generateToken.spec.ts
@@ -2,12 +2,13 @@ import jwt from 'jsonwebtoken';
 import generateToken from '../authentication/generateToken';
 import { jwtStruct } from '../../types/jwt';
 
-const payload = { username: 'JohnLukeThe3rd' };
+const payload = { username: 'JohnLukeThe3rd', role: 'admin' };
 
 describe('generating a token', () => {
   it('should generate a jwt', () => {
     const token = generateToken(payload);
     const decoded: jwtStruct = jwt.decode(token) as jwtStruct;
     expect(decoded.username).toEqual(payload.username);
+    expect(decoded.role).toEqual(payload.role);
   });
 });
