69efe2845276c5f57421541e6242652735b900be
kibartas <juodasis.perlas@gmail.com>
2020-07-29 14:33:09 +0300

    change jwt.verify to generateToken

--- src/__tests__/authMiddleware.spec.ts
+++ src/__tests__/authMiddleware.spec.ts
@@ -3,16 +3,21 @@ import express, {
 } from 'express';
 
 import request from 'supertest';
-import jwt from 'jsonwebtoken';
 
 import { auth } from '../middleware/auth';
 import { appInit } from '../testUtils/appInit';
+import generateToken from '../authentication/generateToken';
+
+const userData = {
+  username: 'JohnLukeThe3rd',
+  role: 'admin',
+};
 
 const prep = (app: Application, mockedFunction: jest.Mock) => {
   app.use(mockedFunction);
   app.use(auth);
   app.get('/test', (req: Request, res: Response, next: NextFunction): void => {
-    res.json({ username: req.username });
+    res.json({ username: req.username, role: req.role });
     next();
   });
 };
@@ -36,10 +41,7 @@ describe('authentication middleware function', () => {
   it('should return 401 if there\'s no "Bearer " in front of token', (done) => {
     mockedFunction = jest.fn(
       (req: Request, res: Response, next: NextFunction): void => {
-        req.headers.authorization = jwt.sign(
-          { username: 'JohnLukeThe3rd' },
-          process.env.TOKEN_SECRET as string,
-        );
+        req.headers.authorization = generateToken(userData);
         next();
       },
     );
@@ -50,12 +52,10 @@ describe('authentication middleware function', () => {
   it('should return 401 if token is malformed', (done) => {
     mockedFunction = jest.fn(
       (req: Request, res: Response, next: NextFunction): void => {
-        req.headers.authorization = `Bearer ${jwt
-          .sign(
-            { username: 'JohnLukeThe3rd' },
-            process.env.TOKEN_SECRET as string,
-          )
-          .slice(0, -2)}`;
+        req.headers.authorization = `Bearer ${generateToken(userData)}`.slice(
+          0,
+          -2,
+        );
         next();
       },
     );
@@ -63,13 +63,10 @@ describe('authentication middleware function', () => {
     request(app).get('/test').expect({}).expect(401, done);
   });
 
-  it("should return 200 and username if everything's okay", (done) => {
+  it("should return 200, username and role if everything's okay", (done) => {
     mockedFunction = jest.fn(
       (req: Request, res: Response, next: NextFunction): void => {
-        req.headers.authorization = `Bearer ${jwt.sign(
-          { username: 'JohnLukeThe3rd' },
-          process.env.TOKEN_SECRET as string,
-        )}`;
+        req.headers.authorization = `Bearer ${generateToken(userData)}`;
         next();
       },
     );
@@ -77,6 +74,6 @@ describe('authentication middleware function', () => {
     request(app)
       .get('/test')
       .expect(200)
-      .expect({ username: 'JohnLukeThe3rd' }, done);
+      .expect({ username: 'JohnLukeThe3rd', role: 'admin' }, done);
   });
 });
