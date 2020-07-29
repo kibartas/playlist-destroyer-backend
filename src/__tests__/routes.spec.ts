45bd5f133958dc0b062aaa91859a9296f772a665
kibartas <juodasis.perlas@gmail.com>
2020-07-29 21:56:34 +0300

    rewrite tests for routes/index.ts because of upgrades to the endpoint logic

--- src/__tests__/routes.spec.ts
+++ src/__tests__/routes.spec.ts
@@ -1,37 +1,52 @@
 import request from 'supertest';
-import express, { Application } from 'express';
+import express, {
+  Application, Request, Response, NextFunction,
+} from 'express';
 
 import jwt from 'jsonwebtoken';
-import initialization from '../initialization';
 import requests from '../routes';
 import { jwtStruct } from '../../types/jwt';
 import generateToken from '../authentication/generateToken';
+import * as db from '../testUtils/db-handler';
+import insertUser from '../database/insertUser';
+import { IUser } from '../../types/user';
+import auth from '../middleware/auth';
+import appInit from '../testUtils/appInit';
 
-const app: Application = express();
-
-const username = 'JohnLukeThe3rd';
+const userData: IUser = {
+  username: 'JohnLukeThe3rd',
+  password: 'Password123',
+  role: 'admin',
+};
 
 describe('routing', () => {
-  beforeAll(() => {
-    initialization(app);
-    requests(app);
-  });
+  beforeAll(async () => db.connect());
+  afterEach(async () => db.clearDatabase());
+  afterAll(async () => db.closeDatabase());
 
   describe('POST /auth', () => {
+    let app: Application;
+    beforeAll(() => {
+      app = express();
+      appInit(app);
+      requests(app);
+    });
+
     it('should respond with a jwt token and username', async () => {
+      await insertUser(userData);
       const response = await request(app)
         .post('/auth')
-        .send({ username: 'JohnLukeThe3rd', password: 'abc' })
+        .send({ username: userData.username, password: userData.password })
         .set('Accept', 'application/json')
         .expect(200);
       const decoded: jwtStruct = jwt.verify(
         response.body.jwt,
         process.env.TOKEN_SECRET as string,
       ) as jwtStruct;
-      expect(decoded.username).toEqual(username);
+      expect(decoded.username).toEqual(userData.username);
     });
 
-    it('should respond with a 404 Bad Request', (done) => {
+    it('should respond with a 400 Bad Request', (done) => {
       request(app)
         .post('/auth')
         .send({ ba: 'friga', passord: 'jesus' })
@@ -39,16 +54,100 @@ describe('routing', () => {
         .expect('Bad request')
         .expect(400, done);
     });
+
+    it('should respond with a 422 because of wrong username', (done) => {
+      request(app)
+        .post('/auth')
+        .send({ username: 'friga', password: 'jesus' })
+        .set('Accept', 'application/json')
+        .expect(422, done);
+    });
+
+    it('should respond with a 422 because of wrong password', async (done) => {
+      await insertUser(userData);
+      request(app)
+        .post('/auth')
+        .send({ username: userData.username, password: 'jesus' })
+        .set('Accept', 'application/json')
+        .expect(422, done);
+    });
   });
 
+  const prep = (
+    app: Application,
+    mockedFunction: jest.Mock = jest.fn(
+      (req: Request, res: Response, next: NextFunction) => {
+        next();
+      },
+    ),
+  ) => {
+    appInit(app);
+    app.use(auth);
+    app.use(mockedFunction);
+    requests(app);
+  };
+
   describe('GET /user', () => {
-    it('should respond with user information', (done) => {
+    let app: Application;
+    beforeEach(() => {
+      app = express();
+    });
+
+    it('should respond with user information', async () => {
+      prep(app);
+      await insertUser(userData);
+      const response = await request(app)
+        .get('/user')
+        .set(
+          'Authorization',
+          `Bearer ${generateToken({
+            username: userData.username,
+            role: userData.role,
+          })}`,
+        )
+        .set('Accept', 'application/json');
+      expect(response.status).toEqual(200);
+      expect(response.body).toEqual({
+        username: userData.username,
+        role: userData.role,
+      });
+    });
+
+    it('should respond with 500 because of bad middleware management', (done) => {
+      const badMiddleware = jest.fn(
+        (req: Request, res: Response, next: NextFunction) => {
+          req.username = undefined;
+          next();
+        },
+      );
+      prep(app, badMiddleware);
       request(app)
         .get('/user')
-        .set('Authorization', `Bearer ${generateToken({ username })}`)
+        .set(
+          'Authorization',
+          `Bearer ${generateToken({
+            username: userData.username,
+            role: userData.role,
+          })}`,
+        )
         .set('Accept', 'application/json')
-        .expect(200)
-        .expect({ username }, done);
+        .expect(500, done);
+    });
+
+    it('should respond with 500 because of database problems', async (done) => {
+      prep(app);
+      await db.clearDatabase();
+      request(app)
+        .get('/user')
+        .set(
+          'Authorization',
+          `Bearer ${generateToken({
+            username: userData.username,
+            role: userData.role,
+          })}`,
+        )
+        .set('Accept', 'application/json')
+        .expect(500, done);
     });
   });
 });
