3056e3030aeba4d6069a75f1dcfcafa36cbca31b
kibartas <juodasis.perlas@gmail.com>
2020-07-29 12:49:45 +0300

    update user model

--- src/models/user.ts
+++ src/models/user.ts
@@ -4,6 +4,8 @@ export interface IUser extends Document {
   username: string;
   password: string;
   creationDate: Date;
+  lastLogin: Date;
+  role: 'admin' | 'user';
 }
 
 const userSchema = new mongoose.Schema({
@@ -11,6 +13,7 @@ const userSchema = new mongoose.Schema({
   password: { type: String, required: true },
   creationDate: { type: Date, default: new Date(), required: true },
   lastLogin: Date,
+  role: { type: String, enum: ['user', 'admin'], default: 'user' },
 });
 
 export default mongoose.model<IUser>('User', userSchema);
