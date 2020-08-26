ece8868093846e740f2808dbd85fc9a17ae889f3
kibartas <juodasis.perlas@gmail.com>
2020-08-26 18:28:53 +0300

    add cookie-parser to initialization of the app

--- src/initialization/index.ts
+++ src/initialization/index.ts
@@ -3,6 +3,7 @@ import * as dotenv from 'dotenv';
 import * as bodyParser from 'body-parser';
 import cors from 'cors';
 import helmet from 'helmet';
+import cookieParser from 'cookie-parser';
 import { tokenValidation } from '../middleware/tokenValidation';
 import { unless } from '../middleware/utils/unless';
 import requests, { routes } from '../routes';
@@ -15,6 +16,7 @@ export default async (app: Application): Promise<void> => {
   app.use(cors());
 
   app.use(helmet());
+  app.use(cookieParser());
 
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
